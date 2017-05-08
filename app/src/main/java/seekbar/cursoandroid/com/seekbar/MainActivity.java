6e64086a3776219aef1e93290aed2873c40f308a
Carlos Bruno <cbcarlos08@gmail.com>
2017-05-08 14:22:25 -0400

    Curso de Android

--- app/src/main/java/seekbar/cursoandroid/com/seekbar/MainActivity.java
+++ app/src/main/java/seekbar/cursoandroid/com/seekbar/MainActivity.java
@@ -1,13 +1,38 @@
 package seekbar.cursoandroid.com.seekbar;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    private SeekBar seekbar;
+    private TextView textoExibicao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekbar = (SeekBar) findViewById(R.id.seekBarId);
+        textoExibicao = (TextView) findViewById(R.id.textoExibicaoId);
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textoExibicao.setText("Progresso: "+progress + " / "+ seekBar.getMax());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(), "Seekbar pressionado", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(), "Salvar o valor do seekbar", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
